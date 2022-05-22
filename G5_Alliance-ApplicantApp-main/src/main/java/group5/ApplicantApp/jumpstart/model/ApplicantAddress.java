bddbd4df5fcf301d2f6ef74f92e2a195469823a9
Ariannemae Alocillo <56349732+ariannemaealocillo@users.noreply.github.com>
2022-05-23 07:48:31 +0800

    Add files via upload

--- G5_Alliance-ApplicantApp-main/src/main/java/group5/ApplicantApp/jumpstart/model/ApplicantAddress.java
+++ G5_Alliance-ApplicantApp-main/src/main/java/group5/ApplicantApp/jumpstart/model/ApplicantAddress.java
@@ -1,131 +1,125 @@
-//package group5.ApplicantApp.jumpstart.model;
-//
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.FetchType;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
-//import javax.persistence.OneToMany;
-//import javax.persistence.PrePersist;
-//import javax.persistence.Table;
-//
-//import org.springframework.format.annotation.DateTimeFormat;
-//
-//
-//
-//@Entity
-//@Table(name = "applicant_address")
-//public class ApplicantAddress {
-//	
-//	@Id
-//	@GeneratedValue(strategy =  GenerationType.IDENTITY)
-//	private long id;
-//	
-//	@Column(name = "city_address")
-//	private String city_address;
-//	
-//	@Column(name = "provincial_address")
-//	private String provincial_address;
-//
-//	@ManyToOne
-//	@JoinColumn(name = "applicant_id")
-//	private Applicant applicant;
-//	
-//	public ApplicantAddress()
-//	{
-//		
-//	}
-//	
-//	
-//	
-//	
-//	
-//	public ApplicantAddress(String city_address, String provincial_address, Applicant applicant) {
-//		
-//		this.city_address = city_address;
-//		this.provincial_address = provincial_address;
-//		this.applicant = applicant;
-//	}
-//
-//
-//
-//
-//
-//	public long getId() {
-//		return id;
-//	}
-//
-//
-//
-//
-//
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//
-//
-//
-//
-//
-//	public String getCity_address() {
-//		return city_address;
-//	}
-//
-//
-//
-//
-//
-//	public void setCity_address(String city_address) {
-//		this.city_address = city_address;
-//	}
-//
-//
-//
-//
-//
-//	public String getProvincial_address() {
-//		return provincial_address;
-//	}
-//
-//
-//
-//
-//
-//	public void setProvincial_address(String provincial_address) {
-//		this.provincial_address = provincial_address;
-//	}
-//
-//
-//
-//
-//
-//	public Applicant getApplicant() {
-//		return applicant;
-//	}
-//
-//
-//
-//
-//
-//	public void setApplicant(Applicant applicant) {
-//		this.applicant = applicant;
-//	}
-//
-//
-//	
-//
-//	
-//	@Override
-//	public String toString()
-//	{
-//		return city_address + " " + provincial_address;
-//	}
-//}
+package group5.ApplicantApp.jumpstart.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+
+@Entity
+@Table(name = "applicant_address")
+public class ApplicantAddress {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private long app_id;
+	
+	@Column(name = "city_address")
+	private String city_address;
+	
+	@Column(name = "provincial_address")
+	private String provincial_address;
+
+	@ManyToOne
+	@JoinColumn(name = "app_id", referencedColumnName = "app_id", insertable=false, updatable=false, nullable=false)
+	private ApplicantModel applicant;
+	
+	public ApplicantAddress()
+	{
+		
+	}
+	
+
+	public ApplicantAddress(String city_address, String provincial_address, ApplicantModel applicant) {
+		
+		this.city_address = city_address;
+		this.provincial_address = provincial_address;
+		this.applicant = applicant;
+	}
+
+
+
+
+
+	public long getId() {
+		return app_id;
+	}
+
+
+
+
+
+	public void setId(long id) {
+		this.app_id = id;
+	}
+
+
+
+
+
+	public String getCity_address() {
+		return city_address;
+	}
+
+
+
+
+
+	public void setCity_address(String city_address) {
+		this.city_address = city_address;
+	}
+
+
+
+
+
+	public String getProvincial_address() {
+		return provincial_address;
+	}
+
+
+
+
+
+	public void setProvincial_address(String provincial_address) {
+		this.provincial_address = provincial_address;
+	}
+
+
+
+	public ApplicantModel getApplicant() {
+		return applicant;
+	}
+
+
+
+
+	public void setApplicant(ApplicantModel applicant) {
+		this.applicant = applicant;
+	}
+
+
+	
+
+
+	@Override
+	public String toString()
+	{
+		return city_address + " " + provincial_address;
+	}
+}
---
+++ G5_Alliance-ApplicantApp-main/src/main/java/group5/ApplicantApp/jumpstart/model/ApplicantModel.java
@@ -0,0 +1,119 @@
+package group5.ApplicantApp.jumpstart.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+
+
+@Entity
+@Table(name = "applicants")
+public class ApplicantModel {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private long app_id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "job_position")
+	private String jobpos;
+	
+	@Column(name = "status")
+	private String status;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dateApplied;
+	
+	@PrePersist
+	private void onCreate()
+	{
+		dateApplied = new Date();
+	}
+	
+	
+	@OneToMany(mappedBy = "applicant",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+	private List<ApplicantAddress>  appAddress = new ArrayList<>();
+	
+	public Date getDateApplied() {
+		return dateApplied;
+	}
+	public void setDateApplied(Date dateApplied) {
+		this.dateApplied = dateApplied;
+	}
+	public long getId() {
+		return app_id;
+	}
+	public void setId(long id) {
+		this.app_id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getJobpos() {
+		return jobpos;
+	}
+
+	public void setJobpos(String jobpos) {
+		this.jobpos = jobpos;
+	}
+	
+	
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public List<ApplicantAddress> getAppAddress() {
+		return appAddress;
+	}
+	public void setAppAddress(List<ApplicantAddress> appAddress) {
+		this.appAddress = appAddress;
+	}
+	
+	public void addAddress(String city_address, String provincial_address) {
+		this.appAddress.add(new ApplicantAddress(city_address, provincial_address,this));
+	}
+	
+	
+}
